827eed118c9b87268977b086f560f2a17d463ad7
Ilya <stoynov.oleg@gmail.com>
2025-01-14 12:47:03 +0100

    coomit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import {Hero,About,Contact,Services} from "@/components/index"
+import {Hero,About,Contact} from "@/components/index"
 
 export default function Home() {
   return (
     <div>
       <Hero/>
       <About/>
-      {/* <Services/> */}
       <Contact/>
     </div>
   );
